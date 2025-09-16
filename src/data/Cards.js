92496a134d60b24b716c0fcb9744054781614bf5
Aditya <adityapanigrahy98@gmail.com>
2025-09-16 21:53:23 +0530

    added the cards data

---
+++ src/data/Cards.js
@@ -0,0 +1,75 @@
+// src/data/cards.js
+export const cards = [
+  {
+    id: 1,
+    defaultSrc:
+      "https://framerusercontent.com/images/xJ4VI5dc1aKjWzvqscHOZwOEOiU.jpg?scale-down-to=512&width=4768&height=7151",
+    hoverSrc:
+      "https://framerusercontent.com/images/BSaz0fXWU2uFOAYeuZvwhgP7Rk.jpeg?width=928&height=1160",
+    alt: "Fluffy rabbit on a floral chair",
+  },
+  {
+    id: 2,
+    defaultSrc:
+      "https://framerusercontent.com/images/V3IvVt18lBJ6hNuki7Bct7LtLl4.jpg?width=3738&height=4987",
+    hoverSrc:
+      "https://framerusercontent.com/images/DUGrdR9WgdBilQ33iHslUzlPI.jpg?width=2848&height=3792",
+    alt: "Lotus flower against cloudy sky",
+  },
+  {
+    id: 3,
+    defaultSrc:
+      "https://framerusercontent.com/images/RBiQ5RAej3M9Gq9cWtbbjDr8A.jpg?width=4002&height=5995",
+    hoverSrc:
+      "https://framerusercontent.com/images/9YeROwasiM7f0xtJSYY5sQFMe38.jpg?width=4000&height=6000",
+    alt: "Lone tree silhouette at sunset",
+  },
+  {
+    id: 4,
+    defaultSrc:
+      "https://framerusercontent.com/images/8S3RcbIfQCwsuxt3ZWXeslItug.jpg?width=4806&height=7209",
+    hoverSrc:
+      "https://framerusercontent.com/images/YIUdKBPd3rYNLcdUAifT9GIptRk.jpg?width=3024&height=4032",
+    alt: "Palm tree on a sandy beach",
+  },
+  {
+    id: 5,
+    defaultSrc:
+      "https://framerusercontent.com/images/gDSlQDS8fFiHxjdEZtfM644AMWg.jpg?width=3630&height=5445",
+    hoverSrc:
+      "https://framerusercontent.com/images/c57bYwLV7EcqLU8pMQwEXJeWZs.jpg?width=2267&height=3400",
+    alt: "Dark green baseball cap",
+  },
+  {
+    id: 6,
+    defaultSrc:
+      "https://framerusercontent.com/images/KJ7anciccQf5MJJV7cwiYDCHY.jpg?width=3002&height=4503",
+    hoverSrc:
+      "https://framerusercontent.com/images/Iwer8B9yXTymcKsFLjOWjkRIVw.jpg?width=4000&height=6000",
+    alt: "Chair floating on calm water",
+  },
+  {
+    id: 7,
+    defaultSrc:
+      "https://framerusercontent.com/images/8jObj2TAwGjeJMxk3CCLectyk.jpg?width=3661&height=4724",
+    hoverSrc:
+      "https://framerusercontent.com/images/nDEAquzEDeb4ziygK2VqKU9U.jpg?width=3649&height=5444",
+    alt: "Abstract painting with bold brushstrokes",
+  },
+  {
+    id: 8,
+    defaultSrc:
+      "https://framerusercontent.com/images/4Hg0sv7400Yx05RGwp6wEX0Jw.jpg?width=3042&height=4565",
+    hoverSrc:
+      "https://framerusercontent.com/images/57VisoKOEOA90s1lPzg75ZoBij0.jpg?width=3757&height=4696",
+    alt: "Vintage camera on wood surface",
+  },
+  {
+    id: 9,
+    defaultSrc:
+      "https://framerusercontent.com/images/Gwvzsn9kru4RI1wsvNjTLrF6Z8.jpg?width=2844&height=3902",
+    hoverSrc:
+      "https://framerusercontent.com/images/urmm5uCcz46SZuSeWussPnbfl4.jpg?width=3240&height=4050",
+    alt: "Modern living room with TV",
+  },
+];
